0d57a66b5e8c3226e0c5d97fb668119d2ede6528
Saran <50368466+saran-23@users.noreply.github.com>
2022-10-16 20:41:01 +0530

    Update pymongo.py

--- pymongo.py
+++ pymongo.py
@@ -1,5 +1,30 @@
-import pymongo
-from pymongo import MongoClient
+# import pymongo
+# from pymongo import MongoClient
 
-mongo_connection = pymongo.MongoClient('localhost', 27017)
-db = mongo_connection['test']
+# mongo_connection = pymongo.MongoClient('localhost', 27017)
+# db = mongo_connection['test']
+
+
+import pymongo
+
+# CONNECTION
+mongo_connect = pymongo.MongoClient('localhost', 27017)
+
+# DATABASE
+db = mongo_connect["pymongo"]
+
+# COLLECTION
+col = db["employees"]
+
+# DATA (DICT)
+data = {
+    "name": "Saran",
+    "course": "Computer Science",
+    "Number": 44646464644
+}
+# INSERTING DATA TO THE COLLECTION
+# insert_data = col.insert_one(data)
+
+# VIEWING COLLECTION
+find = col.find_one()
+print(find)
